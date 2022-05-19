7ca377f4c83db187f9f9762e07c1ff84f9af198b
rohitpatil233000@gmail.com <rohitpatil233000@gmail.com>
2022-05-19 15:07:45 +0530

    first commit

---
+++ OrderItem/OrderItem/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OrderItem.Models;
+
+namespace OrderItem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin,POC")]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetCartById(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44350/api/");
+                var responseTask = client.GetAsync("MenuItem");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                List<Cart> Items = new List<Cart>();
+                if (result.IsSuccessStatusCode)
+                {
+                    string jsonData = result.Content.ReadAsStringAsync().Result;
+                    Items = JsonConvert.DeserializeObject<List<Cart>>(jsonData);
+                    Cart obj1 = Items.SingleOrDefault(x => x.Id == id);
+
+                    obj1.MenuItemId = 1;
+                    obj1.UserId = 1;
+                    return Ok(obj1);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            } 
+        }
+    }
+}
---
+++ OrderItem/OrderItem/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string securityKey = "mysuperdupersecret";
+SymmetricSecurityKey symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        //what to validate
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        //setup validate data
+        ValidIssuer = "mySystem",
+        ValidAudience = "myUsers",
+        IssuerSigningKey = symmetricSecurityKey
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
